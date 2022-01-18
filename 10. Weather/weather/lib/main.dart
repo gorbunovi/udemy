4d9219f10f1505ef9e0126e3d350176a98069b07
Igor <gorbunovi@erambler.com>
2022-01-18 17:06:15 +0400

    created class

--- 10. Weather/weather/lib/main.dart
+++ 10. Weather/weather/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
 
   // API Keys - 1369dd6b5ae78fc9952261ab9aa236b4
+  // url - http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&appid=1369dd6b5ae78fc9952261ab9aa236b4&units=metric
+  //       http://api.openweathermap.org/data/2.5/forecast/daily?&q=London&appid=1369dd6b5ae78fc9952261ab9aa236b4&units=metric
 
   runApp(const MyApp());
 }
